94dfc7df326de25757efdc42fb4fb75fbf0ab2a7
Yasir Obaibullah <yasirobaidullah123@gmail.com>
2022-07-20 18:42:30 +0530

    Add files

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return(
+        <footer>
+            <ul>
+                <li><a rel="noreferrer" href="https://github.com/YasirObaibullah99" target="_blank">Github</a></li>
+                <li><a rel="noreferrer" href="https://github.com/YasirObaibullah99/Fake_Store" target="_blank">Repository</a></li>
+                <li><a rel="noreferrer" href="https://yasirobaibullah99.github.io/PortfolioWebsite" target="_blank">Portfolio</a></li>
+            </ul>
+            <p>Author: Yasir</p>
+        </footer>
+    );
+}
+export default Footer;
